5d575db4a37f50566ea575f6d426710232f87857
Mayank Sharma <buddy.mayank@gmail.com>
2018-01-16 23:35:23 +0530

    updated production environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   clientID: 'DAbylvwgSE68Unu5Q6eWCJfbyq0ajya1',
   domain: 'mayanksharma.auth0.com',
-  callbackURL: 'https://github.com/sharmaguides/get-tweets/callback',
+  callbackURL: 'https://sharmaguides.github.io/get-tweets/callback',
   graphqlURI: 'https://www.graphqlhub.com/graphql'
 };
